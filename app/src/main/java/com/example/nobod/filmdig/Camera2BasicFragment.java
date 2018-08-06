80fdb259290275b1aae8a40481d072cc73807218
Pedro Diniz <aluno19514@ipt.pt>
2018-08-07 00:33:24 +0100

    Não entra em modo de suspensão. E imports optimizados.

--- app/src/main/java/com/example/nobod/filmdig/Camera2BasicFragment.java
+++ app/src/main/java/com/example/nobod/filmdig/Camera2BasicFragment.java
@@ -35,11 +35,8 @@ import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
@@ -52,6 +49,7 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -80,6 +78,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Camera2BasicFragment extends Fragment
         implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
+
     private Handler tirafotoAgora;
 
     void startRepeatingTask() {
